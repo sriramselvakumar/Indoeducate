9ee5b067c12c356a36afd0cf2747e58e261647ee
VishnudevP <vipoil04@gmail.com>
2020-08-14 18:25:38 -0400

    Sign Up Page

--- client/src/Pages/Signup.js
+++ client/src/Pages/Signup.js
@@ -1,7 +1,25 @@
 import React, { useState } from "react";
+import "../CSS/Signup.css"
+import Logo from '../Images/logo.svg'
 
-const Signup = () => {
-  return <h1>This is the sign up page!</h1>;
-};
 
-export default Signup;
+const SignUp = () => {
+  return ( <React.Fragment>
+<div class="wrapper">
+</div>
+<div class="form" id="sign-up-form">
+<div class="logo-container2">
+            <img src= {Logo} alt="Logo"/>
+        </div>
+  <h2 class="form-title">Get started with Indo-Edu</h2>
+  <h6 class="formsubtitle">Enter your information below</h6>
+  <input type="text" class="input-std" id="email" placeholder="Email"/>
+  <input type="password" class="input-std" id="password" placeholder="Password"/>
+  <input type="password" class="input-std" id="password-confirmation" placeholder="Password Confirmation"/>
+  <button type="button" class="btn-submit" id="sign-up">Get Started Today</button>
+  <h6 class="formlast">Already Have an Account? Log In Here.</h6>
+</div>
+  </React.Fragment> );
+}
+ 
+export default SignUp;
