aa4a4185cfc251fb94da1d385793c401fe3a3c18
sriramselvakumar <sriram.selvakumar@outlook.com>
2020-08-14 14:08:22 -0700

    fixed bootstrap bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from './Pages/home'
+import Home from "./Pages/home";
+import SignUp from "./Pages/Signup";
 
 function App() {
-  return (<React.Fragment>
-    <Router>
-      <Switch>
-        <Route path= "/" exact component = {Home} /> 
-      </Switch>
-    </Router>
-  </React.Fragment>);
+  return (
+    <React.Fragment>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/signup" component={SignUp} />
+        </Switch>
+      </Router>
+    </React.Fragment>
+  );
 }
 
 export default App;
---
+++ client/src/Pages/Signup.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+const Signup = () => {
+  return <h1>This is the sign up page!</h1>;
+};
+
+export default Signup;
