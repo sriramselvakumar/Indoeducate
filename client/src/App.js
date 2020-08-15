8e721cefca054720bbe21b97be8494f0031d84a0
VishnudevP <vipoil04@gmail.com>
2020-08-15 15:50:42 -0400

    Updated To merge

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Pages/home";
 import SignUp from "./Pages/Signup";
-import Login from "./Pages/Login";
-import Resources from "./Pages/resources";
+
 
 
 function App() {
@@ -15,8 +14,6 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/signup" component={SignUp} />
-          <Route path="/login" component={Login}/>
-          <Route path="/resources" component={Resources}/>
         </Switch>
       </Router>
     </React.Fragment>
