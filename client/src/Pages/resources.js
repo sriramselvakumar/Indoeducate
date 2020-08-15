377b0457971dec02ee434ed7bf02c1d3223a70b9
VishnudevP <vipoil04@gmail.com>
2020-08-15 09:33:02 -0400

    Updated Resource Page

--- client/src/Pages/resources.js
+++ client/src/Pages/resources.js
@@ -5,31 +5,31 @@ import Logo from '../Images/logo.svg'
 
 const Resources = () => {
     return (<React.Fragment>
-        <header>
-            <div class="logo-container">
+        <div class="headerresource">
+            <div class="logo-container-resource">
                 <img src={Logo} alt="Logo" />
             </div>
-            <nav>
-                <ul class="nav-links">
-                    <li><a class="nav-link" href="#"><p class="nonButton">Upload</p></a></li>
-                    <li><button class="view"><a href="#">View</a></button></li>
-                    <li><a class="nav-link" href="#"><p class="nonButton">Contact Us</p></a></li>
+            <div class="nav-resource">
+                <ul class="nav-links-resource">
+                    <li><a class="nav-link-resource" href="#"><p class="nonButton-resource">Upload</p></a></li>
+                    <li><button class="view-resource"><a href="#">View</a></button></li>
+                    <li><a class="nav-link-resource" href="#"><p class="nonButton-resource">Contact Us</p></a></li>
                 </ul>
-            </nav>
-        </header>
-        <hr class="linetop" />
-        <section class="content">
+            </div>
+        </div>
+        <hr class="linetop-resource" />
+        <section class="content-resource">
             <section class="title">
-                <h1 class="recentUpload">Recently Uploaded</h1>
+                <h1 class="recentUpload-resource">Recently Uploaded</h1>
                 <br></br>
-                <h3 class="subTitle">Click "Open Resources" to access the resources</h3>
+                <h3 class="subTitle-resource">Click "Open Resources" to access the resources</h3>
             </section>
             <br></br><br></br>
             <section class="resources">
                 <ul class="resourceList">
-                    <li class="listItem"><div class="row">
+                    <li class="listItem-resource"><div class="row">
                         <div class="column">
-                            <p>Grade 11 Biology Textbook</p>
+                            <p class="resourcetitle">Grade 11 Biology Textbook</p>
                         </div>
                         <div class="column">
                             <button class="linkSource">
@@ -38,9 +38,9 @@ const Resources = () => {
                         </div>
                     </div>
                     </li>
-                    <li class="listItem"><div class="row">
+                    <li class="listItem-resource"><div class="row">
                         <div class="column">
-                            <p>Grade 11 Biology Textbook</p>
+                            <p class="resourcetitle">Grade 11 Biology Textbook</p>
                         </div>
                         <div class="column">
                             <button class="linkSource">
@@ -49,9 +49,9 @@ const Resources = () => {
                         </div>
                     </div>
                     </li>
-                    <li class="listItem"><div class="row">
+                    <li class="listItem-resource"><div class="row">
                         <div class="column">
-                            <p>Grade 11 Biology Textbook</p>
+                            <p class="resourcetitle">Grade 11 Biology Textbook</p>
                         </div>
                         <div class="column">
                             <button class="linkSource">
@@ -60,9 +60,9 @@ const Resources = () => {
                         </div>
                     </div>
                     </li>
-                    <li class="listItem"><div class="row">
+                    <li class="listItem-resource"><div class="row">
                         <div class="column">
-                            <p>Grade 11 Biology Textbook</p>
+                            <p class="resourcetitle">Grade 11 Biology Textbook</p>
                         </div>
                         <div class="column">
                             <button class="linkSource">
@@ -71,9 +71,9 @@ const Resources = () => {
                         </div>
                     </div>
                     </li>
-                    <li class="listItem"><div class="row">
+                    <li class="listItem-resource"><div class="row">
                         <div class="column">
-                            <p>Grade 11 Biology Textbook</p>
+                            <p class="resourcetitle">Grade 11 Biology Textbook</p>
                         </div>
                         <div class="column">
                             <button class="linkSource">
@@ -82,9 +82,9 @@ const Resources = () => {
                         </div>
                     </div>
                     </li>
-                    <li class="listItem"><div class="row">
+                    <li class="listItem-resource"><div class="row">
                         <div class="column">
-                            <p>Grade 11 Biology Textbook</p>
+                            <p class="resourcetitle">Grade 11 Biology Textbook</p>
                         </div>
                         <div class="column">
                             <button class="linkSource">
@@ -97,13 +97,13 @@ const Resources = () => {
             </section>
         </section>
 
-        <hr class="linefoot" />
+        <hr class="linefoot-resource" />
 
-        <footer>
+        <div class="footer-resource">
 
-            <h4 class="reserved">© 2020 Indo-Edu, Inc. All rights reserved.</h4>
-            <img class="logofoot" src={Logo} alt="Logo" />
-        </footer>
+            <h4 class="reserved-resource">© 2020 Indo-Edu, Inc. All rights reserved.</h4>
+            <img class="logofoot-resource" src={Logo} alt="Logo" />
+        </div>
     </React.Fragment>);
 }
 
